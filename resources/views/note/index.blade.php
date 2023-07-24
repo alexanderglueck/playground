4c361dec7b59b9ac074d72d55f885af1435a1993
Alexander Glück <github@alexanderglueck.at>
2023-07-24 21:10:13 +0200

    Refactor UI

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -5,58 +5,54 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <p>
-                        <a href="{{ route('notebooks.index') }}">{{ __('Notebooks') }}</a><br>
-                        <a href="{{ route('notebooks.create') }}">
-                            {{ __('Create notebook') }}
-                        </a>
-                    </p>
+    <x-panel>
+        <p>
+            <a href="{{ route('notebooks.index') }}">{{ __('Notebooks') }}</a><br>
+            <a href="{{ route('notebooks.create') }}">
+                {{ __('Create notebook') }}
+            </a>
+        </p>
 
-                    <ul>
-                        @foreach($notebooks as $notebook)
-                            <li>
-                                <a href="{{ route('notebooks.show', [$notebook->id]) }}" title="{{ __('View notebook') }}">{{ $notebook->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
+        <ul>
+            @foreach($notebooks as $notebook)
+                <li>
+                    <a href="{{ route('notebooks.show', [$notebook->id]) }}"
+                       title="{{ __('View notebook') }}">{{ $notebook->name }}</a>
+                </li>
+            @endforeach
+        </ul>
 
-                    <hr>
+        <hr>
 
-                    <p>
-                        <a href="{{ route('notes.index') }}">{{ __('Notes') }}</a><br>
-                        <a href="{{ route('notes.create') }}">
-                            {{ __('Create note') }}
-                        </a>
-                    </p>
-                    <ul>
-                        @foreach($notes as $note)
-                            <li>
-                                <a href="{{ route('notes.show', [$note->id]) }}" title="{{ __('View note') }}">{{ $note->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
+        <p>
+            <a href="{{ route('notes.index') }}">{{ __('Notes') }}</a><br>
+            <a href="{{ route('notes.create') }}">
+                {{ __('Create note') }}
+            </a>
+        </p>
+        <ul>
+            @foreach($notes as $note)
+                <li>
+                    <a href="{{ route('notes.show', [$note->id]) }}" title="{{ __('View note') }}">{{ $note->name }}</a>
+                </li>
+            @endforeach
+        </ul>
 
-                    <hr>
-                    <p>
-                        <a href="{{ route('tags.index') }}">{{ __('Tags') }}</a><br>
-                        <a href="{{ route('tags.create') }}">
-                            {{ __('Create tag') }}
-                        </a>
-                    </p>
-                    <ul class="list-inline">
-                        @foreach($tags as $tag)
-                            <li class="list-inline-item">
-                                <a class="badge badge-pill badge-{{ $tag->pill() }}" href="{{ route('tags.show', [$tag->id]) }}" title="{{ __('View tag') }}">{{ $tag->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+        <hr>
+        <p>
+            <a href="{{ route('tags.index') }}">{{ __('Tags') }}</a><br>
+            <a href="{{ route('tags.create') }}">
+                {{ __('Create tag') }}
+            </a>
+        </p>
+        <ul class="list-inline">
+            @foreach($tags as $tag)
+                <li class="list-inline-item">
+                    <a class="badge badge-pill badge-{{ $tag->pill() }}" href="{{ route('tags.show', [$tag->id]) }}"
+                       title="{{ __('View tag') }}">{{ $tag->name }}</a>
+                </li>
+            @endforeach
+        </ul>
+    </x-panel>
 
 </x-app-layout>
