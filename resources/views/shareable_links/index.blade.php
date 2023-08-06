a036a09b75f6693ed2fa78e0b623f5e383f6bb0a
Alexander Glück <github@alexanderglueck.at>
2023-08-07 00:15:53 +0200

    Fix html nesting issue

--- resources/views/shareable_links/index.blade.php
+++ resources/views/shareable_links/index.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Shared Resources') }}
+        </h2>
+    </x-slot>
     <x-panel>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Shared Resources') }}
-            </h2>
-        </x-slot>
 
         <ul>
             @foreach($shareableLinks as $shareableLink)
